timdream/gaia

Bug 799039 - Handle app switching when web activity is completed

apps/system/js/wrapper.js
@@ -7,7 +7,7 @@ var Launcher = (function() {
   }
 
   function currentAppFrame() {
-    return WindowManager.getCurrentDisplayedApp().frame;
+    return WindowManager.getRunningApps(WindowManager.getRunningApps());
   }
 
   var BUTTONBAR_TIMEOUT = 5000;

alivedise/system

js/wrapper.js
@@ -9,7 +9,7 @@ var Launcher = (function() {
   }
 
   function currentAppFrame() {
-    return WindowManager.getCurrentDisplayedApp().frame;
+    return WindowManager.getAppFrame(WindowManager.getDisplayedApp());
   }
 
   var _ = navigator.mozL10n.get;