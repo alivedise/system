michalbe/gaia

Bug 819548 - Remember sys updates are available after reboot
We then prompt for updates on startup

Fixes

New tests and logic

test updated

lint fixes

fixed wrong method name

apps/system/js/update_manager.js
@@ -472,6 +472,7 @@ var UpdateManager = {
 
     if (detail.type && detail.type === 'update-available') {
       this.systemUpdatable.size = detail.size;
+      this.systemUpdatable.persistKnownUpdate();
       this.addToUpdatesQueue(this.systemUpdatable);
     }
   },

apps/system/test/unit/mock_updatable.js
@@ -41,6 +41,7 @@ function MockSystemUpdatable(downloadSize) {
 MockSystemUpdatable.mInstancesCount = 0;
 MockSystemUpdatable.mTeardown = function() {
   MockSystemUpdatable.mInstancesCount = 0;
+  delete MockSystemUpdatable.mKnownUpdate;
 };
 
 
@@ -54,4 +55,18 @@ MockSystemUpdatable.prototype.download = function() {
 
 MockSystemUpdatable.prototype.cancelDownload = function() {
   this.mCancelCalled = true;
+};
+
+MockSystemUpdatable.prototype.persistKnownUpdate = function() {
+  this.mKnownUpdate = true;
+};
+
+MockSystemUpdatable.prototype.forgetKnownUpdate = function() {
+  delete this.mKnownUpdate;
+};
+
+MockSystemUpdatable.prototype.ifKnownUpdate = function(callback) {
+  if (this.mKnownUpdate && typeof callback === 'function') {
+    callback();
+  }
 };

alivedise/system

js/update_manager.js
@@ -472,6 +472,7 @@ var UpdateManager = {
 
     if (detail.type && detail.type === 'update-available') {
       this.systemUpdatable.size = detail.size;
+      this.systemUpdatable.rememberKnownUpdate();
       this.addToUpdatesQueue(this.systemUpdatable);
     }
   },

test/unit/mock_updatable.js
@@ -41,6 +41,7 @@ function MockSystemUpdatable(downloadSize) {
 MockSystemUpdatable.mInstancesCount = 0;
 MockSystemUpdatable.mTeardown = function() {
   MockSystemUpdatable.mInstancesCount = 0;
+  delete MockSystemUpdatable.mKnownUpdate;
 };
 
 
@@ -54,4 +55,18 @@ MockSystemUpdatable.prototype.download = function() {
 
 MockSystemUpdatable.prototype.cancelDownload = function() {
   this.mCancelCalled = true;
+};
+
+MockSystemUpdatable.prototype.rememberKnownUpdate = function() {
+  this.mKnownUpdate = true;
+};
+
+MockSystemUpdatable.prototype.forgetKnownUpdate = function() {
+  delete this.mKnownUpdate;
+};
+
+MockSystemUpdatable.prototype.checkKnownUpdate = function(callback) {
+  if (this.mKnownUpdate && typeof callback === 'function') {
+    callback();
+  }
 };