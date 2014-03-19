mozilla-b2g/gaia

Bug 971577 - [System] Make visibility manager instantiable

apps/system/js/bootstrap.js
@@ -69,6 +69,7 @@ window.addEventListener('load', function startup() {
   window.dialerComms = new DialerComms();
   window.title = new Title();
   window.ttlView = new TTLView();
+  window.visibilityManager = new VisibilityManager().init();
 
   // We need to be sure to get the focus in order to wake up the screen
   // if the phone goes to sleep before any user interaction.

apps/system/test/unit/mock_visibility_manager.js
@@ -1,0 +1,4 @@
+'use strict';
+
+var MockVisibilityManager = function() {};
+MockVisibilityManager.prototype.init = function() {};

alivedise/system

js/bootstrap.js
@@ -74,6 +74,7 @@ window.addEventListener('load', function startup() {
 
   window.title = new Title();
   window.ttlView = new TTLView();
+  window.visibilityManager = new VisibilityManager().start();
 
   // We need to be sure to get the focus in order to wake up the screen
   // if the phone goes to sleep before any user interaction.

test/unit/mock_visibility_manager.js
@@ -1,0 +1,4 @@
+'use strict';
+
+var MockVisibilityManager = function() {};
+MockVisibilityManager.prototype.start = function() {};